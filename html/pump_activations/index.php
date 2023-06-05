97a056e1c6db9d80f67bf3e6d0cab1aa5bb1acea
Your Name <you@example.com>
2023-06-05 19:19:51 +0200

    final website

---
+++ html/pump_activations/index.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html>
+<body style="background-color:#FFF599;text-align:left">
+
+	<head>
+        <p>
+		<h1 style="clear: both;"><img style="vertical-align:middle" src="../waterpump.png" alt="Main" width="100" />Pump activation status</h1>
+        <br>
+        </p>
+</head>
+
+
+
+<head>
+        <p>
+	<h2><FONT COLOR=green>Live pump status:</h2>
+                <img src="current_data.png" />
+        </p>
+
+
+</head>
+
+
+<head>
+        <p>
+	<a title="" href="data/"><h2>Data base</h2>
+        <br>
+        </p>
+</head>
+
+
+
+<a href="../index.php">Go Back</a>
+
+</body>
+</html>
+
